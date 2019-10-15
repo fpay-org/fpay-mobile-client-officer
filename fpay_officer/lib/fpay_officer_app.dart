e64494a01333501121ff7df353fab4ba100873ac
Sashika Nawarathne <cazci269@gmail.com>
2019-10-15 22:04:13 +0530

    Handle routes

--- fpay_officer/lib/fpay_officer_app.dart
+++ fpay_officer/lib/fpay_officer_app.dart
@@ -1,12 +1,20 @@
+import 'package:FPay/routes/application.dart';
+import 'package:FPay/routes/routes.dart';
+import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 
 class FPayOfficerApp extends StatelessWidget {
+  FPayOfficerApp() {
+    final router = Router();
+    Routes.configureRouter(router);
+    Application.router = router;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Officer App",
-      
     );
   }
 }
--- fpay_officer/lib/routes/routes.dart
+++ fpay_officer/lib/routes/routes.dart
@@ -1,7 +1,19 @@
+import 'package:FPay/routes/route_handler.dart';
 import 'package:fluro/fluro.dart';
 
 class Routes {
   static const splash = '/';
   static const auth = '/auth';
   static const home = '/home';
+
+  static void configureRouter(Router router) {
+    router.define(splash,
+        handler: splashHandler, transitionType: TransitionType.native);
+
+    router.define(auth,
+        handler: authHandler, transitionType: TransitionType.native);
+
+    router.define(home,
+        handler: homeHandler, transitionType: TransitionType.native);
+  }
 }

be1cb822f6bb6aad589c1908df33ff5a22f4e9c2
Sashika Nawarathne <cazci269@gmail.com>
2019-10-15 21:49:12 +0530

    Refactor

---
+++ fpay_officer/lib/fpay_officer_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FPayOfficerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Officer App",
+      
+    );
+  }
+}

e7eae421df8a2d25a12432c333fde1771cb23400
Sashika Nawarathne <cazci269@gmail.com>
2020-02-17 22:20:28 +0530

    Implement image upload

--- fpay_officer/lib/services/profile_service.dart
+++ fpay_officer/lib/services/profile_service.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:FPay/config/config.dart';
 import 'package:dio/dio.dart';
 import 'package:logger/logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
-class Officer{
+class Officer {
   String first_name;
   String last_name;
   String officer_id;
@@ -14,7 +14,8 @@ class Officer{
   String nic;
   String police_station;
   String avatar_url;
-  Officer(this.first_name,this.last_name, this.officer_id,this.email,this.contact_number,this.nic,this.police_station,this.avatar_url);
+  Officer(this.first_name, this.last_name, this.officer_id, this.email,
+      this.contact_number, this.nic, this.police_station, this.avatar_url);
 
   // factory Driver.fromJson(Map<String, dynamic> json) {
   //   return Driver(
@@ -27,20 +28,20 @@ class Officer{
   //   );
   // }
 }
+
 class ProfService {
   final baseUrl = Config.baseUrl;
 
-
-  Future<bool> changePass(String officer,String current_password,String new_password){
+  Future<bool> changePass(
+      String officer, String current_password, String new_password) {
     Logger().i("$officer");
     Logger().i("$current_password");
     Logger().i("$new_password");
 
     return Dio().post('$baseUrl/pass/$officer', data: {
-      "officer":officer,
-      "password":current_password,
-      "new_password":new_password
-      
+      "officer": officer,
+      "password": current_password,
+      "new_password": new_password
     }).then((res) async {
       if (res.statusCode == 201) {
         Logger().i("${res.statusCode}");
@@ -52,10 +53,17 @@ class ProfService {
       return false;
     });
   }
-  
-  Future<bool> editDetails(String officer,String first_name,String last_name,String email,String contact_nummber,String nic,String police_station,String password,){
 
-
+  Future<bool> editDetails(
+    String officer,
+    String first_name,
+    String last_name,
+    String email,
+    String contact_nummber,
+    String nic,
+    String police_station,
+    String password,
+  ) {
     Logger().i("$officer");
     Logger().i("$first_name");
     Logger().i("$last_name");
@@ -65,18 +73,15 @@ class ProfService {
     Logger().i("$police_station");
     Logger().i("$password");
 
-
     return Dio().post('$baseUrl/officer/$officer', data: {
-      "officer":officer,
-      "first_name":first_name,
-      "last_name":last_name,
-      "email":email,
-      "contact_number":contact_nummber,
-      "nic":nic,
-      "police_station":police_station,
-      "password":password
-
-      
+      "officer": officer,
+      "first_name": first_name,
+      "last_name": last_name,
+      "email": email,
+      "contact_number": contact_nummber,
+      "nic": nic,
+      "police_station": police_station,
+      "password": password
     }).then((res) async {
       if (res.statusCode == 202) {
         Logger().i("${res.statusCode}");
@@ -88,7 +93,7 @@ class ProfService {
       return false;
     });
   }
- 
+
   Future<Officer> getDetails() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String token = prefs.getString("token");
@@ -96,15 +101,27 @@ class ProfService {
     Officer officer;
     Logger().i("pus ${token}");
 
-   return Dio().get('$baseUrl/me?token=$token',).then((res) async {
+    return Dio()
+        .get(
+      '$baseUrl/me?token=$token',
+    )
+        .then((res) async {
       Logger().i("$res");
-      
+
       if (res.statusCode == 200) {
         Logger().i("prof: ${res.data["data"]}");
         var f = res.data["data"];
         Logger().i("f: ${f["avatar_url"]}");
         //print(res);
-        officer  = Officer(f["first_name"],f["last_name"],f["officer_id"],f["email"],f["contact_number"],f["nic"],f["police_station"],f["avatar_url"]);
+        officer = Officer(
+            f["first_name"],
+            f["last_name"],
+            f["officer_id"],
+            f["email"],
+            f["contact_number"],
+            f["nic"],
+            f["police_station"],
+            f["avatar_url"]);
         Logger().i("d: ${officer}");
         //driver.add(driver);
         //String token = res.data["data"]["token"];
@@ -112,34 +129,51 @@ class ProfService {
         Logger().i("prof");
         return officer;
       }
-    Logger().i("returned false");
-      
+      Logger().i("returned false");
+
       return officer;
     }).catchError((err) => officer);
   }
-  
+
   Future<bool> _saveToken(String token) {
     return SharedPreferences.getInstance().then((instance) {
       return instance.setString("token", token);
     });
   }
 
-  Future <String> _getToken(){
+  Future<String> _getToken() {
     return SharedPreferences.getInstance().then((instance) {
       return instance.getString("token");
     });
   }
 
-  Future<bool> logout()  async {
+  Future<bool> logout() async {
     //BuildContext context;
-    await SharedPreferences.getInstance().then((prefs){
-          prefs.remove("token");
-          //Application.router.navigateTo(context,'/',clearStack: true);
-          
+    await SharedPreferences.getInstance().then((prefs) {
+      prefs.remove("token");
+      //Application.router.navigateTo(context,'/',clearStack: true);
     });
-Logger().i("logout");
+    Logger().i("logout");
     return true;
   }
 
-}
+  Future<bool> updateAvatar(File image) {
+    return getDetails().then((currentOfficer) async {
+      FormData _data = FormData.fromMap({
+        "officer_image":
+            await MultipartFile.fromFile(image.path, filename: "test"),
+      });
 
+      return Dio()
+          .post('$baseUrl/officer/${currentOfficer.officer_id}/avatar',
+              data: _data)
+          .then((res) {
+        if (res.statusCode == 202) return true;
+        return false;
+      }).catchError((error) {
+        Logger().e(error);
+        return false;
+      });
+    });
+  }
+}
